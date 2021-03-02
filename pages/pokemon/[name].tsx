7068d390f541de9a34cf9e0fd360985becddf3a9
Bryan Rivera <brivera@applaudostudios.com>
2021-03-02 11:35:47 -0600

    initial commit

---
+++ pages/pokemon/[name].tsx
@@ -0,0 +1,88 @@
+import { Col, Container, Row } from 'react-bootstrap'
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Pokemon as PokemonType } from 'models'
+
+import pokemon from 'pokemon.json'
+
+type Props = {
+  data: PokemonType
+}
+
+function Pokemon({ data }: Props) {
+  return (
+    <>
+      <div>
+        <Head>
+          <title>{(data ? data.name.english : null) || 'Pokemon'}</title>
+        </Head>
+
+        <Container>
+          {data ? (
+            <>
+              <Row>
+                <Col xs={6}>
+                  <h1>{data.name.english}</h1>
+
+                  <br />
+
+                  {Object.entries(data.base).map(([key, value]) => (
+                    <Row key={key}>
+                      <Col xs={3}>
+                        <p>{key}</p>
+                      </Col>
+                      <Col xs={4}>
+                        <h5>{value}</h5>
+                      </Col>
+                    </Row>
+                  ))}
+                </Col>
+
+                <Col xs={6}>
+                  <Image
+                    src={`/pokemon/${data.name.english
+                      .toLowerCase()
+                      .replace(' ', '-')}.jpg`}
+                    height={350}
+                    width={350}
+                  />
+                </Col>
+              </Row>
+            </>
+          ) : null}
+
+          <Link href="/">
+            <a>
+              <h3>Return to Home</h3>
+            </a>
+          </Link>
+        </Container>
+      </div>
+
+      <style jsx>{`
+        div {
+          padding: 3rem;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: pokemon.map(({ name: { english } }) => ({
+    params: { name: english }
+  })),
+  fallback: false
+})
+
+export const getStaticProps: GetStaticProps = async ({ params }) => ({
+  props: {
+    data: pokemon.filter(({ name: { english } }) => english === params?.name)[0]
+  }
+})
+
+export default Pokemon
