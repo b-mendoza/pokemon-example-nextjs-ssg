9a8e88a5d5648241c4ad64d73f1a7b738bd33dc9
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-15 03:23:14 -0600

    feat: update `_document` component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,12 +6,13 @@ class __Document extends Document {
       <Html>
         <Head>
           <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
             crossOrigin="anonymous"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            rel="stylesheet"
           />
         </Head>
+
         <body>
           <Main />
           <NextScript />
