179790c0b2e25bebb42b283880ec80877f4c1457
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-15 17:52:10 -0600

    feat: update `search` API route

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -3,23 +3,21 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import pokemon from 'pokemon.json';
 
-type Data = Pokemon[];
+type Data = {
+  pokemonList: Pokemon[];
+};
 
 export default function searchPokemon(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
+  const typedPokemonList = pokemon as Pokemon[];
+
   const filter = req.query.q ? new RegExp(req.query.q as string, 'i') : /.*/;
 
-  res.statusCode = 200;
-
-  res.setHeader('Content-Type', 'application/json');
-
-  res.end(
-    JSON.stringify(
-      pokemon
-        .filter(({ name: { english } }) => filter.exec(english))
-        .slice(0, 10),
-    ),
-  );
+  res.status(200).json({
+    pokemonList: typedPokemonList
+      .filter(({ name: { english } }) => filter.exec(english))
+      .slice(0, 10),
+  });
 }
